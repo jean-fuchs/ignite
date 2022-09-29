7ac61bea3dafec98dc1c1ccce2f9ac8bddf31ed8
Jean Carlos Fuchs Cordeiro <prcarlosjean@gmail.com>
2022-09-29 19:58:25 -0300

    finapi first commit

---
+++ finapi/src/index.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const { v4: uuidv4 } = require("uuid")
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already existes!" });
+  };
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" })
+  }
+
+  return response.status(200).json(customer.statement);
+});
+
+app.listen(3333);
